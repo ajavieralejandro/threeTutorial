7c67f0131d8a81b5e7f27ddbdb55da6171fcbf50
ajavieralejandro <amorosijavier@gmail.com>
2024-04-07 13:04:03 -0300

    Direction Light Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import {Canvas} from '@react-three/fiber'
 import Watch from './components/watch'
 import { OrbitControls } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
 function App() {
 
   return (
     <>
-    <Canvas>
+    <Canvas gl={{preserveDrawingBuffer:true,phisicallyCorrectLight:true}}>
+
       <color attach='background' args={['lightblue']} />
+
       <OrbitControls />
-      <ambientLight intensity={1} />
+
+      <directionalLight position={[3.3, 4.0, 4.4]} intensity={4} />
       <Watch />
     </Canvas>
+    
+    </>
+  )
+}
+
+const Scene = () =>{
+  return(
+    <>
     
     </>
   )
